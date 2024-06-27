a8fa7b4bbccf15f396f8ca7893b88b5de0332d3e
taha <jamtaha170@gmail.com>
2024-06-27 03:48:06 +0330

    first commit

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <section>
+      <input type="text" class="task" placeholder="ENTER YOUR TASK" v-model="InputTodo">
+      <button @click="addTodo()" class="btn">ADD</button> 
+ 
+    </section>
+    <ul class="list-q" v-for="todo in todolist" :key="todo.id">
+      <li class="list" v-if="todo!=''">
+        {{ todo }}
+      <button class="btn.danger" @click="() => deletetodo()">
+        DELETE
+      </button>
+        
+      </li>
+
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  // props: {
+  //   msg: String
+  // }
+data (){
+  return{
+    InputTodo:'',
+    todolist: [''],
+  }
+},
+
+methods:{
+  addTodo() {
+     this.todolist.push(this.InputTodo)
+     this.InputTodo= ""
+  },
+   deletetodo(){
+        const index = this.todolist.indexOf(this.InputTodo)
+        if(index > -1){
+          this.todolist.splice(1, index)
+        }
+   }
+}
+}
+
+</script>
+
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+
+</style>
